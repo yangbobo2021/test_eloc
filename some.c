08ed5ef78e615144c8637ded666fef0499ba1b24
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-10-27 14:27:41 +0800

    Create  some.c

---
+++ some.c
@@ -0,0 +1,12 @@
+static int compute3(int c, int d,int e) {
+  if (e > d) {
+    return e;
+  }
+  if (c > d) {
+    return c + d;
+  } else if (c == d) {
+    return c * d;
+  } else {
+    return c - d;
+  }
+}
